b714b88beb5a2d77f5ad3a9b0b5f15b1ff63b629
Jhardel <jpumahuillca@esan.edu.pe>
2026-02-25 11:41:40 -0500

    Primera Clase de Vue

---
+++ Clase01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ num }}</h1>
+    <button @click="incrementar" class="btn btn-success">INCREMENTAR</button>
+    <button @click="disminuir" class="btn btn-danger">DISMINUIR</button>
+    <Card :link="'https://www.vuemastery.com/pricing/?coupon=40off_2025'" :title="'TITULO1'" :content="'Contenido 123'" :name-buton="'CESAR '"/>
+    <Card :link="'https://www.vuemastery.com/pricing/?coupon=40off_2025'" :title="'4123123'" :content="'Contenido 12413123'"/>
+    <Card :link="'https://www.vuemastery.com/pricing/?coupon=40off_2025'" :title="'Titulo3'" :content="'Contenido 4'"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Card from './components/Card.vue';
+
+  import { ref } from 'vue';
+
+  const num = ref(0);
+
+  const incrementar = () =>{
+    num.value++;
+  }
+    const disminuir = () =>{
+    num.value <= 0 ? alert ("Ya no puedes bajar mas") : num.value--;
+  }
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Clase01/src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ content }}</p>
+            <a :href="link" class="btn btn-primary">{{ nameButon || "Enviar" }}</a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    title: string,
+    content: string,
+    link: string,
+    nameButon?: string
+}>();
+
+</script>
+
+<style scoped></style>
